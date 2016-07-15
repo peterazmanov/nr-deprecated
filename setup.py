16db0b5a6ea36814456acd827b255feb9d50dadd
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-07-15 15:39:29 +0200

    initial commit with nr.utils.recordclass

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+  name = 'nr.utils',
+  version = '0.0.1.dev',
+  author_name = 'Niklas Rosenstein',
+  author_email = 'rosensteinniklas@gmail.com',
+  license = 'MIT',
+  packages = ['nr.utils'],
+)
