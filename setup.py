1ac2afb25158eb081300a0ece2101a6dc3077304
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-09-16 23:55:19 +0200

    Prepare 1.1.1 release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
   name = 'nr.utils',
-  version = '1.1',
+  version = '1.1.1',
   author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   license = 'MIT',
