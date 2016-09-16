6e8cc4f5ad638449e97cb3b5cf756ea8d737d397
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-09-16 19:13:06 +0200

    fix author option in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
   name = 'nr.utils',
   version = '1.1',
-  author_name = 'Niklas Rosenstein',
+  author = 'Niklas Rosenstein',
   author_email = 'rosensteinniklas@gmail.com',
   license = 'MIT',
   packages = ['nr.utils'],
