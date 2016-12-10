8dd050aa2fbd0c1399203539c78fe7d0b29b6b9b
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-12-10 17:13:34 +0100

    remove accidentally commited README.rst, now only generate it with Pandoc when creating a distro (sdist, bdist, bdist_egg, etc.), close #2)

--- setup.py
+++ setup.py
@@ -31,16 +31,20 @@ if pip.__version__ >= '6.0':
   parse_requirements = partial(parse_requirements, session=pip.download.PipSession())
 
 def readme():
-  if os.path.isfile('README.md'):
+  if os.path.isfile('README.md') and any('dist' in x for x in sys.argv[1:]):
     if os.system('pandoc -s README.md -o README.rst') != 0:
       print('-----------------------------------------------------------------')
       print('WARNING: README.rst could not be generated, pandoc command failed')
       print('-----------------------------------------------------------------')
       if sys.stdout.isatty():
         input("Enter to continue... ")
+    else:
+      print("Generated README.rst with Pandoc")
 
-  with open('README.rst') as fp:
-    return fp.read()
+  if os.path.isfile('README.rst'):
+    with open('README.rst') as fp:
+      return fp.read()
+  return ''
 
 setup(
   name = 'nr',
