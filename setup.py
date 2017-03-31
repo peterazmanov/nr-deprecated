ed309fd90a8c80eb91e26c42397c7cd14a348eb7
Niklas Rosenstein <rosensteinniklas@gmail.com>
2017-03-31 13:12:25 +0200

    Prepare 1.4.6 release

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ def readme():
 
 setup(
   name = 'nr',
-  version = '1.4.5',
+  version = '1.4.6',
   license = 'MIT',
   description = 'A Collection of small Python libraries.',
   long_description = readme(),
