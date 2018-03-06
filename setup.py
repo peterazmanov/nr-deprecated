3c78bfc7f71f5070660ff7406d47da257537e300
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-03-06 20:03:47 +0100

    Prepare 2.0.1 release.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def readme():
 
 setup(
   name='nr',
-  version='2.0.0',
+  version='2.0.1',
   license='MIT',
   description='Compound utility library and command-line tools for Python 2/3',
   long_description=readme(),
