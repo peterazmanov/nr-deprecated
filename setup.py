e8be6a049ca8ee3438b5885d8fd42e22b268a530
Peter Azmanov <Petr.Azmanov@transas.com>
2018-08-20 17:52:42 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -23,12 +23,15 @@ import os
 import sys
 
 from functools import partial
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup, find_packages
 
 # parse_requirements() interface has changed in Pip 6.0
-if pip.__version__ >= '6.0':
-  parse_requirements = partial(parse_requirements, session=pip.download.PipSession())
+#if pip.__version__ >= '6.0':
+parse_requirements = partial(parse_requirements, session=pip.download.PipSession())
 
 def readme():
   if os.path.isfile('README.md'):
